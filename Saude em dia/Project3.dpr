1cba33cbb80350da07500482ad1b1be9495e3868
HenriqueSolivo~ <henriquesolivo@gmail.com~>
2025-08-14 19:22:26 -0300

    Incluia a tela de caastro de meicamento

--- Saude em dia/Project3.dpr
+++ Saude em dia/Project3.dpr
@@ -1,6 +1,6 @@
 program Project3;
 
-{$R 'Tela_Principal.dfm' :TForm}
+
 
 uses
   Vcl.Forms,
@@ -8,13 +8,16 @@ uses
   Vcl.Styles,
   Saude_em_dia in 'Saude_em_dia.pas' {Form3},
   Tela_Cadastro in 'Tela_Cadastro.pas' {/  Tela_Principal in 'Tela_Principal.pas' {Form4},
-  TTela_Principal in 'Tela_Principal.pas' {TelaPrincipal};
+  TTela_Principal in 'Tela_Principal.pas' {TelaPrincipal},
+  Castro_Meicamento in 'Castro_Meicamento.pas' {Form5},
+  Cadastro_Medicamento in '..\..\..\Saude em dia-20250807T232549Z-1-001\Saude em dia\Cadastro_Medicamento.pas';
 
 {$R *.res}
 
 begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm5, Form5);
   Application.CreateForm(TForm3, Form3);
   Application.CreateForm(TTelaPrincipal, TelaPrincipal);
   TStyleManager.TrySetStyle('Iceberg Classico');
