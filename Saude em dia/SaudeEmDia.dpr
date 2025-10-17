b30f6e18e4faddf3276a4a0060575614b3ff7e58
Gustavo Reinehr <gugahrhr@gmail.com>
2025-10-16 21:03:13 -0300

    Alterações

---
+++ Saude em dia/SaudeEmDia.dpr
@@ -0,0 +1,45 @@
+  program SaudeEmDia;
+
+
+
+uses
+  Vcl.Forms,
+  Vcl.Themes,
+  Vcl.Styles,
+  Saude_em_dia in 'Saude_em_dia.pas' {Form3},
+  TTela_Principal in 'Tela_Principal.pas' {TelaPrincipal},
+  Cadastro_Medicamento in 'Cadastro_Medicamento.pas' {FCadastro_Medicamento},
+  Tela_Cadastro in 'Tela_Cadastro.pas' {Tela_Cadastrp},
+  Tela_Lembrete in 'Tela_Lembrete.pas' {Tela_Lembretes},
+  DataModulePrincipal in 'DataModulePrincipal.pas' {DmPrincipal: TDataModule},
+  DataModuleMeticamento in 'DataModuleMeticamento.pas' {DMMedicamento: TDataModule},
+  DataModuleCadastroPessoas in 'DataModuleCadastroPessoas.pas' {DMCadastroPessoa: TDataModule},
+  DataModuleLembretes in 'DataModuleLembretes.pas' {DataModuleLembrete: TDataModule},
+  DataModuleTelaLogin in 'DataModuleTelaLogin.pas' {DataModuleLogin: TDataModule};
+
+(*uses
+  Vcl.Forms,
+  Vcl.Themes,
+  Vcl.Styles,
+  Saude_em_dia in 'Saude_em_dia.pas' {Form3},
+  TTela_Principal in 'Tela_Principal.pas' {TelaPrincipal},
+
+  Cadastro_Medicamento in 'Cadastro_Medicamento.pas' {FCadastro_Medicamento};*)
+
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm3, Form3);
+  Application.CreateForm(TTelaPrincipal, TelaPrincipal);
+  Application.CreateForm(TTela_Cadastrp, Tela_Cadastrp);
+  Application.CreateForm(TTela_Lembretes, Tela_Lembretes);
+  Application.CreateForm(TFCadastro_Medicamento, FCadastro_Medicamento);
+  Application.CreateForm(TTela_Cadastrp, Tela_Cadastrp);
+  Application.CreateForm(TTela_Lembretes, Tela_Lembretes);
+  Application.CreateForm(TDmPrincipal, DmPrincipal);
+  TStyleManager.TrySetStyle('Iceberg Classico');
+  Application.Run;
+end.
